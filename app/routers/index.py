53959bf59d308952fcde26755c0f44a15ae3d0c3
Rohit Kumar <gahlyanrohit18@gmail.com>
2023-04-22 10:31:22 +0530

    make regex pattern to match filenames & return actual names instead of dummies

--- app/routers/index.py
+++ app/routers/index.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Request, Query
 from fastapi.templating import Jinja2Templates
 import json
+import re
+import os
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
@@ -24,5 +26,23 @@ async def get_subjects(
 
 
 @router.get("/get-files")
-async def get_files():
-    return {"ok": True}
+async def get_files(dept: str, year: str, sem: str, exam: str, subject: str):
+    # [q/a]year-dept-clgyr-sem-t-sub.ext
+
+    pattern = re.compile(
+        r"^q-\d{4}-dept-year-sem-exam-[A-Za-z0-9_-]{5,50}\.pdf$".replace(
+            "dept", dept.upper()
+        )
+        .replace("year", year.upper())
+        .replace("sem", sem.upper())
+        .replace("sub", subject.upper().replace(" ", "_"))
+        .replace("exam", exam.upper())
+    )
+
+    matching_files = []
+    for filename in os.listdir("app/resources"):
+        print(filename)
+        if pattern.match(filename):
+            matching_files.append(filename)
+
+    return matching_files
